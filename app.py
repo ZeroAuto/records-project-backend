fe73a08fa0b6f9a849ee0fe72866437153b24348
Mike James <mikejames@Mikes-Mac-mini.lan>
2023-09-20 14:00:35 -0500

    rename stuff

--- app.py
+++ app.py
@@ -2,44 +2,44 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-stores = [{"name": "My Store", "items": [{"name": "Chair", "price": 15.99}]}]
+records = [{"name": "Marquee Moon", "tracks": [{"name": "See No Evil", "position": 1}]}]
 
 
-@app.get("/store")
-def get_stores():
-    return {"stores": stores}
+@app.get("/record")
+def get_records():
+    return {"records": records}
 
 
-@app.post("/store")
-def create_store():
+@app.post("/record")
+def create_record():
     request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
-    return new_store, 201
+    new_record = {"name": request_data["name"], "tracks": []}
+    records.append(new_record)
+    return new_record, 201
 
 
-@app.post("/store/<string:name>/item")
+@app.post("/record/<string:name>/track")
 def create_item(name):
     request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
+    for record in records:
+        if record["name"] == name:
+            new_item = {"name": request_data["name"], "position": request_data["position"]}
+            record["tracks"].append(new_item)
             return new_item, 201
     return {"message": "Store not found"}, 404
 
 
-@app.get("/store/<string:name>")
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
+@app.get("/record/<string:name>")
+def get_record(name):
+    for record in records:
+        if record["name"] == name:
+            return record
     return {"message": "Store not found"}, 404
 
 
-@app.get("/store/<string:name>/item")
-def get_item_in_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return {"items": store["items"]}
+@app.get("/record/<string:name>/item")
+def get_item_in_record(name):
+    for record in records:
+        if record["name"] == name:
+            return {"tracks": record["tracks"]}
     return {"message": "Store not found"}, 404
