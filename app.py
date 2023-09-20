a9c7dc7fda3ad0c5fff59f832f281d9d082b5495
Mike James <mikejames@Mikes-Mac-mini.lan>
2023-09-20 14:21:17 -0500

    add requirements.txt

--- app.py
+++ app.py
@@ -2,10 +2,23 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-records = [{"name": "Marquee Moon", "tracks": [{"name": "See No Evil", "position": 1}]}]
+# records = [{"name": "Marquee Moon", "tracks": [{"name": "See No Evil", "position": 1}]}]
+records = [
+    {
+        "id": 1,
+        "name": "Marquee Moon",
+        "artist": "Television",
+        "tracks": [
+            {
+                "name": "See No Evil",
+                "position": 1,
+            }
+        ]
+    }
+]
 
 
-@app.get("/record")
+@app.get("/records")
 def get_records():
     return {"records": records}
 
@@ -26,7 +39,7 @@ def create_item(name):
             new_item = {"name": request_data["name"], "position": request_data["position"]}
             record["tracks"].append(new_item)
             return new_item, 201
-    return {"message": "Store not found"}, 404
+    return {"message": "Record not found"}, 404
 
 
 @app.get("/record/<string:name>")
@@ -34,7 +47,7 @@ def get_record(name):
     for record in records:
         if record["name"] == name:
             return record
-    return {"message": "Store not found"}, 404
+    return {"message": "Record not found"}, 404
 
 
 @app.get("/record/<string:name>/item")
@@ -42,4 +55,4 @@ def get_item_in_record(name):
     for record in records:
         if record["name"] == name:
             return {"tracks": record["tracks"]}
-    return {"message": "Store not found"}, 404
+    return {"message": "Record not found"}, 404
