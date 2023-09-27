97d93345a1771f1d127ca8dab952346d52f7a70a
Mike James <mikejames386@gmail.com>
2023-09-27 16:06:47 -0500

    add new models for new tables

---
+++ models/artist.py
@@ -0,0 +1,13 @@
+from db import db
+
+class ArtistModel(db.Model):
+    __tablename__ = "artists"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    records = db.relationship(
+        "RecordModel",
+        back_populates="artist",
+        lazy="dynamic",
+        cascade="all, delete",
+    )
--- models/record.py
+++ models/record.py
@@ -5,7 +5,14 @@ class RecordModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    artist = db.Column(db.String(80), nullable=False)
+    # artist = db.Column(db.String(80), nullable=False)
+    artist_id = db.Column(
+        db.Integer,
+        db.ForeignKey("artist.id"),
+        unique=False,
+        nullable=False
+    )
+    artist = db.relationship("ArtistModel", back_populates="records")
     tracks = db.relationship(
         "TrackModel",
         back_populates="record",
